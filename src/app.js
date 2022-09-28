fb53b63b3b1816b326a520f502ecd0740c592e8d
subhransu sekhar mohanty <54876954+ssmohanty153@users.noreply.github.com>
2022-09-28 16:12:51 +0530

    Added start cmd

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const hbs=require('hbs');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const static_path = path.join(__dirname, "../public");
 
